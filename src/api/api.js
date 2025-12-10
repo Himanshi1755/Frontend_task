bbcae1b38bf100890ce90242be7186abc41cb95f
Himashi <himanshijoshi11d8655@gmail.com>
2025-12-10 16:33:49 +0530

    Changes in api

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://backend-task-wwn5.onrender.com/app", 
+  baseURL: "https://backend-task-1-hgla.onrender.com/app", 
 });
+
